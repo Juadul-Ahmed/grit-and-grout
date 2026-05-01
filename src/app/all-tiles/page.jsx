9b69d49fc4433b5af1aa7013ba4ee52e02ba1724
juadul <parallaxhub@gmail.com>
2026-05-01 17:20:08 +0800

    All-tiles page added

--- src/app/all-tiles/page.jsx
+++ src/app/all-tiles/page.jsx
@@ -1,11 +1,17 @@
+import TileCard from '@/components/TileCard';
 import React from 'react';
 
-const TileDetailsPage = () => {
+const AllTilePage = async () => {
+  const res = await fetch("https://69f42960bd2396bf5310a64a.mockapi.io/tiles");
+  const tiles = await res.json();
   return (
     <div>
-      
+      <h1 className='text-3xl font-bold text-center mt-4 mb-6'>All Tiles</h1>
+      <div className='grid lg:grid-cols-3 md:grid-cols-2 gap-4'>
+        {tiles.map(tile => <TileCard key={tile.id} tile={tile} />)}
+      </div>
     </div>
   );
 };
 
-export default TileDetailsPage;
+export default AllTilePage;
